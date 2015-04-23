66747623484bf055ab71ee67bdfbb8e03657d7b5
Roland Tritsch <roland@tritsch.org>
2015-04-23 06:03:22 +0100

    Adding iOS app

---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,37 @@
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace Tasky.iOS {
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate {
+		UIWindow window;
+		UINavigationController navController;
+		UITableViewController homeViewController;
+
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options) {
+			// create a new window instance based on the screen size
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			
+			// make the window visible
+			window.MakeKeyAndVisible ();
+			
+			// create our nav controller
+			navController = new UINavigationController ();
+
+			// create our home controller based on the device
+			if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone) {
+				homeViewController = new Screens.HomeScreen ();
+			} else {
+				// sample does not contain an iPad UI, so re-use the iPhone one for now; this just shows how you might support iPad
+				homeViewController = new Screens.HomeScreen ();
+			}
+
+			// push the view controller onto the nav controller and show the window
+			navController.PushViewController(homeViewController, false);
+			window.RootViewController = navController;
+			window.MakeKeyAndVisible ();
+			
+			return true;
+		}
+	}
+}
